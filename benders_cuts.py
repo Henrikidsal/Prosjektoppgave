bc6e586797fd59557ff349a592ee077cd6dfc632
Henrik Idsal <henrikidsal00@gmail.com>
2024-12-06 12:30:48 +0100

    w

--- benders_cuts.py
+++ benders_cuts.py
@@ -1,21 +1,21 @@
 from pyomo.environ import *
 
-def generate_benders_cut(master, dual_values, theta_j, master_var_values):
+def generate_benders_cut(master, m, dual_values, theta_j, master_var_values):
 
     LHS = master.beta
     RHS = theta_j
 
-    
     for constraint, dual_dict in dual_values.items(): 
         for (g, t), dual_value in dual_dict.items():
             if abs(dual_value) < 1e-6:
                 continue
             if constraint == "fixing_ug":
-                RHS += dual_value * (master.ug[g, t] - master_var_values["ug"][g, t])
+                RHS += abs(dual_value) * (master.ug[g, t] - master_var_values["ug"][g, t])
             elif constraint == "fixing_vg":
-                RHS += dual_value * (master.vg[g, t] - master_var_values["vg"][g, t])
+                RHS += abs(dual_value) * (master.vg[g, t] - master_var_values["vg"][g, t])
             elif constraint == "fixing_wg":
-                RHS += dual_value * (master.wg[g, t] - master_var_values["wg"][g, t])
+                RHS += abs(dual_value) * (master.wg[g, t] - master_var_values["wg"][g, t])
+
 
     # Add the Benders cut to the master problem
     master.benders_cuts.add(LHS >= RHS)
